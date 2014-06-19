9d726d60539411889be06e1f86a9ac7bc93ee668
Scott <Scott@Shiny.local>
2014-06-19 14:07:08 -0400

    Oops, finished Creating a sub-response Feature Test.

--- test/features/responses_test.rb
+++ test/features/responses_test.rb
@@ -34,23 +34,21 @@ feature 'Creating a sub-response' do
 		page.must_have_css 'form'
 	end
 	
-	scenario 'Submitting form does not create new question if invalid' do
-#		visit new_question_path
-#		fill_in 'question[summary]', with: ''
-#		
-#		refute_difference('Question.count') { click_on 'Ask' }
+	scenario 'Submitting form does not create new response if invalid' do
+		fill_in 'response[summary]', with: ''
+		
+		refute_difference('Response.count') { click_on 'Publish' }
 	end
 	
-#	scenario 'Submitting form creates new question if valid' do
-#		visit new_question_path
-#		summary = 'This question summary has a question mark?'
-#		fill_in 'question[summary]', with: summary
-#		body = 'And here is a question body'
-#		fill_in 'question[body]', with: body
-#		
-#		assert_difference('Question.count') { click_on 'Ask' }
-#		current_path.must_equal question_path(Question.count)
-#		page.must_have_css '#question .summary', text: summary
-#		page.must_have_css '#question .body', text: body
-#	end
+	scenario 'Submitting form creates new response if valid' do
+		summary = 'This is a response summary.'
+		fill_in 'response[summary]', with: summary
+		body = 'And this is a response body'
+		fill_in 'response[body]', with: body
+		
+		assert_difference('Response.count') { click_on 'Publish' }
+		current_path.must_equal node_path(@response)
+		page.must_have_css '.response .summary', text: summary
+		page.must_have_css '.response .body', text: body
+	end
 end
